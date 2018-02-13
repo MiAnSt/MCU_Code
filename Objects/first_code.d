5601e9756ac143f4720fb5de6f2a0e202c8a2f86
HaMaMiPe <mikolajj@stud.ntnu.no>
2018-02-13 15:41:48 +0100

    First code

---
+++ Objects/first_code.d
@@ -0,0 +1,1 @@
+.\objects\first_code.o: first_code.c
---
+++ first_code.c
@@ -0,0 +1,13 @@
+#include "clock_config.h"
+#include "fsl_common.h"                 
+#include "fsl_port.h"                   
+#include "fsl_gpio.h"                   
+
+
+
+
+int main(void)
+{
+
+
+}
